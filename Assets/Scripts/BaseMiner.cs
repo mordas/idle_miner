2e0222efe31a82ac4053140eace1ae84b5d1ab0b
dMordas <mordasdsp@gmail.com>
2021-01-07 16:04:02 +0300

    BaseMiner class and ShaftMiner

---
+++ Assets/Scripts/BaseMiner.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class BaseMiner : MonoBehaviour
+{
+    private float moveSpeed = 5.0f;
+    private int inititalCollectCompacity = 200;
+    private float goldCollectPerSecond = 50.0f;
+
+public void MoveMiner(Vector3 newPosition){
+    transform.DOMove(newPosition, 10/moveSpeed).Play();
+}
+}
---
+++ Assets/Scripts/ShaftMiner.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShaftMiner : BaseMiner
+{
+    [SerializeField] private Transform shaftMiningLocation;
+    void Update()
+    {
+       if (Input.GetKeyDown(KeyCode.M))
+       {
+        MoveMiner(shaftMiningLocation.position);   
+       } 
+    }
+}
