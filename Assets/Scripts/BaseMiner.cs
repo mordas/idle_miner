2e1a039d01af65958d957728564ad113d6f7b970
dMordas <mordasdsp@gmail.com>
2021-01-08 14:22:20 +0300

    CollectGold

--- Assets/Scripts/BaseMiner.cs
+++ Assets/Scripts/BaseMiner.cs
@@ -6,10 +6,40 @@ using DG.Tweening;
 public class BaseMiner : MonoBehaviour
 {
     private float moveSpeed = 5.0f;
-    private int inititalCollectCompacity = 200;
+    private int inititalCollectCapacity = 200;
     private float goldCollectPerSecond = 50.0f;
 
-public void MoveMiner(Vector3 newPosition){
-    transform.DOMove(newPosition, 10/moveSpeed).Play();
-}
+    public int CurrentGold { get; set; }
+    public int CollectCapacity { get; set; }
+    public float CollectPerSecond { get; set; }
+    public bool IsTimeToCollect { get; set; }
+    void Awake()
+    {
+        IsTimeToCollect = true;
+        CurrentGold = 0;
+        CollectCapacity = inititalCollectCapacity;
+        CollectPerSecond = goldCollectPerSecond;
+    }
+    public void MoveMiner(Vector3 newPosition)
+    {
+        transform.DOMove(newPosition, 10 / moveSpeed).OnComplete((() =>
+        {
+            if (IsTimeToCollect)
+            {
+                CollectGold();
+            }
+
+
+        })).Play();
+    }
+
+    protected virtual void CollectGold()
+    {
+
+    }
+    protected virtual IEnumerator IECollect(int collectGold, float collectTime)
+    {
+        yield return null;
+
+    }
 }
--- Assets/Scripts/ShaftMiner.cs
+++ Assets/Scripts/ShaftMiner.cs
@@ -5,11 +5,23 @@ using UnityEngine;
 public class ShaftMiner : BaseMiner
 {
     [SerializeField] private Transform shaftMiningLocation;
+    [SerializeField] private Transform shaftDepositLocation;
     void Update()
     {
-       if (Input.GetKeyDown(KeyCode.M))
-       {
-        MoveMiner(shaftMiningLocation.position);   
-       } 
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            MoveMiner(shaftMiningLocation.position);
+        }
+    }
+    protected override void CollectGold()
+    {
+        float collectTime = CollectCapacity / CollectPerSecond;
+StartCoroutine(IECollect(CollectCapacity, collectTime));
+    }
+    protected override IEnumerator IECollect(int collectGold, float collectTime)
+    {
+        yield return new WaitForSeconds(collectTime);
+        CurrentGold = collectGold;
+        MoveMiner(shaftDepositLocation.position);
     }
 }
