312fce9b28a89c08f2184cceafd218918731c3d6
dMordas <mordasdsp@gmail.com>
2021-01-15 22:12:22 +0300

    UI + animations

---
+++ Assets/Scripts/ShaftUI.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class ShaftUI : MonoBehaviour
+{
+   [SerializeField] private TextMeshProUGUI currentGoldTMP;
+   private Shaft _shaft;
+   void Start()
+   {
+   _shaft = GetComponent<Shaft>()    ;
+   }
+   void Update()
+   {
+   currentGoldTMP.text = _shaft.CurrentDeposit.CurrentGold.ToString();    
+   }
+}
